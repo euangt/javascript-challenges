6f752065efb3d7539f62701b386ccb2b94060757
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-12-17 15:35:33 +0000

    conventions ordered

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,9 @@
-let js = "amazing"
-if (js === "amazing") alert("hello world")
+let js = "amazing";
+if (js === "amazing") alert("hello world");
+
+const unchangeable = "unchangeable variable";
+let changeable = "changeable variable";
+
+console.log(unchangeable);
+console.log("Javascript uses camelCase but variables cannot be declared with a number as the initial character");
+console.log("Keep you variable names descriptive to meet convention and make life easier");
